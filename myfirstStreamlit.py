ce2b904ec87c5fec566df99e384fda7c9f7c863a
Atindranath Mukherjee <125726396+atindranathm@users.noreply.github.com>
2025-07-22 21:11:29 +0530

    Update myfirstStreamlit.py

--- myfirstStreamlit.py
+++ myfirstStreamlit.py
@@ -1,5 +1,3 @@
 import streamlit as st
-st.write("Atin has just started learning Streamlit")
-st.write("Tojo is a good boy !!!")
-st.write("Riya has a pet dog")
-st.write("Riya & Riyan are best friends")
+st.write("Always be honest")
+
